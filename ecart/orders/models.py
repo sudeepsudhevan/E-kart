3c33efa818c1dfd822bc18750a9747279a6c3609
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-01-04 14:45:17 +0530

    order model added

--- ecart/orders/models.py
+++ ecart/orders/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from customers.models import Customer
+from products.models import Product
 
 # Create your models here.
+
+
+class Order(models.Model):
+    LIVE = 1
+    DELETE = 0
+    DELETE_CHOICES = (
+        (LIVE, "Live"),
+        (DELETE, "Delete"),
+    )
+    CART_STAGE = 0
+    ORDER_CONFIRMED = 1
+    ORDER_PROCESSED = 2
+    ORDER_DELIVERED = 3
+    ORDER_REJECTED = 4
+    STATUS_CHOICES = (
+        (ORDER_PROCESSED, "ORDER_PROCESSED"),
+        (ORDER_DELIVERED, "ORDER_DELIVERED"),
+        (ORDER_REJECTED, "ORDER_REJECTED"),
+    )
+    order_status = models.IntegerField(choices=STATUS_CHOICES, default=CART_STAGE)
+
+    owner = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, related_name="orders"
+    )
+    delete_status = models.IntegerField(choices=DELETE_CHOICES, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class OrderedItem(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, related_name="added_carts"
+    )
+    quantity = models.IntegerField(default=1)
+    owner = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="added_items"
+    )
