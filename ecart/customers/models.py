df9a35266ea27ddd4ba5406d2e00b28fbf70d164
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-01-04 14:19:03 +0530

    customer model added

--- ecart/customers/models.py
+++ ecart/customers/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    LIVE = 1
+    DELETE = 0
+    DELETE_CHOICES = (
+        (LIVE, "Live"),
+        (DELETE, "Delete"),
+    )
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="customer_profile"
+    )
+    phone = models.CharField(max_length=15)
+    delete_status = models.IntegerField(choices=DELETE_CHOICES, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
