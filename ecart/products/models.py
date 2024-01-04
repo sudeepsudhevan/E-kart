058f64bd33a09b76f2dd430de479212217b18375
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-01-04 14:18:21 +0530

    product model added

--- ecart/products/models.py
+++ ecart/products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+# Model for products
+
+
+class Product(models.Model):
+    LIVE = 1
+    DELETE = 0
+    DELETE_CHOICES = (
+        (LIVE, "Live"),
+        (DELETE, "Delete"),
+    )
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    description = models.TextField()
+    image = models.ImageField(upload_to="/media")
+    priority = models.IntegerField(default=0)
+    delete_status = models.IntegerField(choices=DELETE_CHOICES, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
