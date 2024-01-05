5bd60578ae76a1a151d6d7d378c2bf0d692757d7
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-01-05 13:02:17 +0530

    added url.py and views.py

---
+++ ecart/products/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index),
+]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
