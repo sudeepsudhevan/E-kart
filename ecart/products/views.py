5fa5b023e8a55afa664eb29f651c34424f6eb1c3
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-01-11 17:13:17 +0530

    paginaion included

--- ecart/products/views.py
+++ ecart/products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from .models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -19,7 +19,13 @@ def list_products(request):
     Returns:
         _type_: _description_
     """
+    page = 1
+    if request.GET:
+        page = request.GET.get("page", 1)
+
     product_list = Product.objects.all()
+    product_paginator = Paginator(product_list, 2)
+    product_list = product_paginator.get_page(page)
     context = {"products": product_list}
     return render(request, "products.html", context)
 
