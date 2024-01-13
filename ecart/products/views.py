489f6b1a1ed6b35638afa6c1995f4188d4a7347a
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-01-13 09:36:50 +0530

    Home page data added from the bakend

--- ecart/products/views.py
+++ ecart/products/views.py
@@ -6,7 +6,14 @@ from django.core.paginator import Paginator
 
 
 def index(request):
-    return render(request, "index.html")
+    featured_products = Product.objects.order_by("priority")[:4]
+    latest_products = Product.objects.order_by("-id")[:4]
+    context = {
+        "featured_products": featured_products,
+        "latest_products": latest_products,
+    }
+    # print(context)
+    return render(request, "index.html", context)
 
 
 def list_products(request):
