97c7f10962810afa985edb2914b4a6fb0b5b3918
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-01-17 11:28:48 +0530

    added login and authentication menu

--- ecart/customers/urls.py
+++ ecart/customers/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("account", views.show_account, name="account"),
+    path("logout", views.sign_out, name="logout"),
 ]
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecart/customers/views.py
+++ ecart/customers/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Customer
 from django.contrib import messages
 
 # Create your views here.
 
 
+def sign_out(request):
+    logout(request)
+    return redirect("home")
+
+
 def show_account(request):
+    context = {}
     if (
         request.POST and "register" in request.POST
     ):  # request.POST is a dictionary, and register is a key in the dictionary
+        context["register"] = True
         try:
             username = request.POST.get("username")
             password = request.POST.get("password")
@@ -19,7 +27,7 @@ def show_account(request):
             # print(username, password, email, address, phone)
 
             # create users accounts
-            user = User.objects.create(
+            user = User.objects.create_user(
                 username=username,
                 password=password,
                 email=email,
@@ -32,10 +40,24 @@ def show_account(request):
                 phone=phone,
                 user=user,
             )
-            return redirect("home")
+            success_message = "Account created successfully"
+            messages.success(request, success_message)
         except Exception as e:
             print(e)
             error_message = "Duplicate username or invalid email"
             messages.error(request, error_message)
 
-    return render(request, "account.html")
+    if request.POST and "login" in request.POST:
+        context["register"] = False
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            error_message = "Invalid username or password"
+            messages.error(request, error_message)
+
+    return render(request, "account.html", context)
