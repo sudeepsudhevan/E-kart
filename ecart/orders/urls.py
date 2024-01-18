72ae925c3f98d5eeb12a70de11762e4ec4f01070
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-01-18 11:04:04 +0530

    added cart page

--- ecart/orders/urls.py
+++ ecart/orders/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("cart", views.show_cart, name="cart"),
+    path("add_to_cart", views.add_to_cart, name="add_to_cart"),
 ]
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecart/orders/views.py
+++ ecart/orders/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Order, OrderedItem
+from products.models import Product
 
 # Create your views here.
 
 
 def show_cart(request):
-    return render(request, "cart.html")
+    user = request.user
+    customer = user.customer_profile
+    cart_obj, created = Order.objects.get_or_create(
+        owner=customer, order_status=Order.CART_STAGE
+    )
+    context = {"cart": cart_obj}
+    return render(request, "cart.html", context)
+
+
+def add_to_cart(request):
+    if request.method == "POST":
+        user = request.user
+        customer = user.customer_profile
+        quantity = int(request.POST.get("quantity"))
+        product_id = request.POST.get("product_id")
+        cart_obj, created = Order.objects.get_or_create(
+            owner=customer, order_status=Order.CART_STAGE
+        )
+        product = Product.objects.get(pk=product_id)
+        ordered_item, created = OrderedItem.objects.get_or_create(
+            product=product, owner=cart_obj
+        )
+        if created:
+            ordered_item.quantity = quantity
+            ordered_item.save()
+        else:
+            ordered_item.quantity += quantity
+            ordered_item.save()
+
+    return redirect("cart")
